0a7ec52c0848606c9f1e694cb79726128b099c03
Hemmyevo <xxxlildevil999@gmail.com>
2025-11-30 14:53:09 +0100

    update backend for vercel

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,10 @@
 from app import create_app
 
 app = create_app()
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app = create_app()
+    app.run(debug=True)
+
+# For Vercel serverless import
+else:
+    app = create_app('production')
